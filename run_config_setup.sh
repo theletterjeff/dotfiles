3d54ea8c379d3be778b2e77ea3a1f962d225df0a
Jeff Martin <jeffrey.j.martin2@gmail.com>
2023-11-28 09:42:01 -0500

    add run config setup script

---
+++ run_config_setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# clone or update nvchad repo
+if [ -d "$HOME/.config/nvim" ]; then
+    pushd "$HOME/.config/nvim" && git pull origin && popd
+else
+    git clone https://github.com/NvChad/NvChad "$HOME/.config/nvim" --depth 1 && nvim
+fi
+
+# add symlink to nvchad_custom
+if ![ -L "$HOME/.config/nvim/lua/custom" ]; then
+    ln -s "$HOME/nvchad_custom" "$HOME/.config/nvim/lua/custom"
+fi

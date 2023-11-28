57a9d5af805c1184ebd7a6c020e0c86464ddcda3
Jeff Martin <jeffrey.j.martin2@gmail.com>
2023-11-28 09:39:46 -0500

    add nvchad custom

---
+++ .nvchad_custom/init.lua
@@ -0,0 +1,14 @@
+vim.o.number = true
+vim.o.relativenumber = true
+vim.opt.colorcolumn = "80"
+vim.o.statusline = vim.o.statusline .. "%f"
+
+-- indents
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "sh",
+    callback = function()
+        vim.opt_local.tabstop = 4
+        vim.opt_local.shiftwidth = 4
+        vim.opt_local.expandtab = true -- Use spaces instead of tabs
+    end,
+})

26377c776007b1d9754e1a0466e750796ffaf1a8
Jeff Martin <jmartin@vistarmedia.com>
2024-03-28 10:10:19 -0400

    theme up (tokyodark), working on telescope

--- .config/nvim/lua/setup.lua
+++ .config/nvim/lua/setup.lua
@@ -1,4 +1,6 @@
-print(vim.fn.stdpath("data") .. "/lazy/lazy.nvim")
+vim.g.mapleader = " "
+vim.g.localmapleader = " "
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -12,13 +14,8 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.runtimepath:prepend(lazypath)
 
-local theme = require("themes").current_theme
-
 require("lazy").setup("plugins", {
   install = {
-    colorscheme = { theme.current_theme },
-  },
-  diff = {
-    cmd = "terminal_git",
+    colorscheme = { require("themes").current_theme },
   },
 })
--- .config/nvim/lua/themes.lua
+++ .config/nvim/lua/themes.lua
@@ -1,5 +1,7 @@
 local M = {}
 
+local current_theme = "tokyonight"
+
 M.themes = {
   tokyonight = {
     package = "folke/tokyonight.nvim",
@@ -9,8 +11,31 @@ M.themes = {
       vim.cmd.colorscheme("tokyonight-night")
     end,
   },
+	catppuccin = {
+		package = "catppuccin/nvim",
+		package_name = "catppuccin",
+		name = "catppuccin",
+		config = function(palette)
+			require("catppuccin").setup({
+				flavour = palette or "mocha",
+				custom_highlights = function(colors)
+					return {
+						cjsxElement = { link = "@constructor" },
+						cjsxAttribProperty = { link = "@tag.attribute" },
+						MatchParenCur = { fg = colors.yellow, style = { "bold" } },
+						MatchParen = { bg = colors.base, fg = colors.yellow, style = { "bold" } },
+						LspInlayHint = { link = "Comment" },
+					}
+				end,
+			})
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
 }
 
-M.current_theme = M.themes["tokyonight"]
+M.current_theme = M.themes[current_theme]
+if M.current_theme == M.themes["catppuccin"] then
+  M.current_theme.palette = "mocha"
+end
 
 return M

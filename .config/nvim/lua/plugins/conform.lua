cf24f0d0fbe2a4f3213eff5990dcc6449fbce202
Jeff Martin <jmartin@vistarmedia.com>
2024-04-02 20:34:06 -0400

    run bazel formatter targets on save for scala, py, go

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -2,8 +2,24 @@ return {
 	"stevearc/conform.nvim",
 	config = function()
 		local conform = require("conform")
-
 		local util = require("conform.util")
+
+    -- take a bazel formatter target and optional args and return a table that
+    -- can be passed to a formatter key
+    local formatter_config = function(bazel_target, ...)
+      local unpack = unpack or table.unpack
+      local args = (
+      ... and {"run", bazel_target, unpack({...})}
+      ) or {"run", bazel_target }
+
+      return {
+        command = "bazel",
+        args = args,
+        stdin = true,
+        cwd = util.root_file("WORKSPACE"),
+        require_cwd = true,
+      }
+    end
 
 		conform.setup({
 			formatters_by_ft = {
@@ -11,39 +27,21 @@ return {
         go = { "gofmt" },
 				json = { "jq" },
 				lua = { "stylua" },
-				python = { { "pyfmt", "black" } },
+				python = { "pyfmt" },
         scala = { "scalafmt" },
 				typescript = { "prettier" },
 				typescriptreact = { "prettier" },
 				["_"] = { "trim_whitespace" },
 			},
+			formatters = {
+				pyfmt = formatter_config("//tools/pyfmt"),
+        gofmt = formatter_config("//:gofmt", "--", "$FILENAME"),
+				scalamft = formatter_config("//tools/scalafmt", "--", "$PWD/$FILENAME"),
+			},
 			format_on_save = {
 				lsp_fallback = true,
 			},
 			notify_on_error = true,
-			formatters = {
-				pyfmt = {
-					command = "bazel",
-					args = { "run", "//tools/pyfmt" },
-					stdin = true,
-					cwd = util.root_file("WORKSPACE"),
-					require_cwd = true,
-				},
-				scalamft = {
-					command = "bazel",
-					args = { "run", "//tools/scalafmt" },
-					stdin = true,
-					cwd = util.root_file("WORKSPACE"),
-					require_cwd = true,
-				},
-				gofmt = {
-					command = "bazel",
-					args = { "run", "//:gofmt", "--", "-w" },
-					stdin = true,
-					cwd = util.root_file("WORKSPACE"),
-					require_cwd = true,
-				},
-			},
 		})
 	end,
 }

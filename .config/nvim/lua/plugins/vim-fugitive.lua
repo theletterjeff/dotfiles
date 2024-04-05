1f4e7f35db732b56a1cb564b65cb5a5f280afb90
Jeff Martin <jeffrey.j.martin2@gmail.com>
2024-04-04 20:23:59 -0400

    add esc-esc fugitive quit keymap

--- .config/nvim/lua/plugins/vim-fugitive.lua
+++ .config/nvim/lua/plugins/vim-fugitive.lua
@@ -1,4 +1,12 @@
 return {
   "tpope/vim-fugitive",
   lazy = false,
+  keys = {
+    {
+      "<esc><esc>",
+      ":<C-U>if bufnr('$') == 1<Bar>quit<Bar>else<Bar>bdelete<Bar>endif<CR>",
+      desc = "Exit fugitive window",
+      silent = true,
+    },
+  },
 }

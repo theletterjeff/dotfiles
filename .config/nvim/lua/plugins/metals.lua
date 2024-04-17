7deff0145a132283ea0d21285f38583efa28ee81
Jeff Martin <jmartin@vistarmedia.com>
2024-04-17 14:58:16 -0400

    add metals lsp

---
+++ .config/nvim/lua/plugins/metals.lua
@@ -0,0 +1,21 @@
+return {
+  "scalameta/nvim-metals",
+  config = function()
+    local present, metals = pcall(require, "metals")
+
+    if not present then
+      error("No metals :(")
+      return
+    end
+
+    vim.cmd([[
+  augroup lsp
+    au!
+    au FileType scala,sbt lua require("metals").initialize_or_attach({})
+  augroup end
+]])
+
+    METALS_CONFIG = metals.bare_config()
+    METALS_CONFIG.init_options.statusBarProvider = "on"
+  end,
+}

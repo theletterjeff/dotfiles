397751775e979a73ca1d4bae2e144609ebb8f703
Jeff Martin <jmartin@vistarmedia.com>
2024-04-24 10:32:45 -0400

    refactor formatters

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,16 +1,16 @@
 return {
-	"stevearc/conform.nvim",
-	config = function()
-		local conform = require("conform")
-		local util = require("conform.util")
+  "stevearc/conform.nvim",
+  config = function()
+    local conform = require("conform")
+    local util = require("conform.util")
 
     -- take a bazel formatter target and optional args and return a table that
     -- can be passed to a formatter key
     local formatter_config = function(bazel_target, ...)
       local unpack = unpack or table.unpack
       local args = (
-      ... and {"run", bazel_target, unpack({...})}
-      ) or {"run", bazel_target }
+        ... and { "run", bazel_target, unpack({ ... }) }
+      ) or { "run", bazel_target }
 
       return {
         command = "bazel",
@@ -21,27 +21,27 @@ return {
       }
     end
 
-		conform.setup({
-			formatters_by_ft = {
-				bzl = { "buildifier" },
+    conform.setup({
+      formatters_by_ft = {
+        bzl = { "buildifier" },
         go = { "gofmt" },
-				json = { "jq" },
-				lua = { "stylua" },
-				python = { "pyfmt" },
+        json = { "jq" },
+        lua = { "stylua" },
+        python = { "pyfmt" },
         scala = { "scalafmt" },
-				typescript = { "prettier" },
-				typescriptreact = { "prettier" },
-				["_"] = { "trim_whitespace" },
-			},
-			formatters = {
-				pyfmt = formatter_config("//tools/pyfmt"),
-        gofmt = formatter_config("//:gofmt", "--", "$FILENAME"),
-				scalamft = formatter_config("//tools/scalafmt", "--", "$PWD/$FILENAME"),
-			},
-			format_on_save = {
-				lsp_fallback = true,
-			},
-			notify_on_error = true,
-		})
-	end,
+        typescript = { "prettier" },
+        typescriptreact = { "prettier" },
+        ["_"] = { "trim_whitespace" },
+      },
+      formatters = {
+        pyfmt = formatter_config("//tools/pyfmt"),
+        -- gofmt = formatter_config("//:gofmt", "--", "$PWD/$FILENAME"),
+        scalamft = formatter_config("//tools/scalafmt", "--", "$PWD/$FILENAME"),
+      },
+      format_on_save = {
+        lsp_fallback = true,
+      },
+      notify_on_error = true,
+    })
+  end,
 }

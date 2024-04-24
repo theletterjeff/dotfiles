6ba4d4cfa5f88caff9d4579b77cf218c8f14b2c5
Jeff Martin <jmartin@vistarmedia.com>
2024-04-24 10:34:11 -0400

    replace alex m metals config with bare config from repo

--- .config/nvim/lua/plugins/metals.lua
+++ .config/nvim/lua/plugins/metals.lua
@@ -1,21 +1,29 @@
 return {
   "scalameta/nvim-metals",
-  config = function()
-    local present, metals = pcall(require, "metals")
-
-    if not present then
-      error("No metals :(")
-      return
-    end
-
-    vim.cmd([[
-  augroup lsp
-    au!
-    au FileType scala,sbt lua require("metals").initialize_or_attach({})
-  augroup end
-]])
-
-    METALS_CONFIG = metals.bare_config()
-    METALS_CONFIG.init_options.statusBarProvider = "on"
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    {
+      "j-hui/fidget.nvim",
+      opts = {},
+    },
+  },
+  ft = { "scala", "sbt", "java" },
+  opts = function()
+    local metals_config = require("metals").bare_config()
+    metals_config.settings = {
+      showImplicitArguments = true,
+    }
+    metals_config.init_options.statusBarProvider = "off"
+    return metals_config
   end,
+  config = function(self, metals_config)
+    local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = self.ft,
+      callback = function()
+        require("metals").initialize_or_attach(metals_config)
+      end,
+      group = nvim_metals_group,
+    })
+  end
 }
